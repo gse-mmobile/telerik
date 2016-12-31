3acbdf7ba1723968843901c94430b6d088243198
krankuba <krankuba@gmail.com>
2016-12-31 19:45:26 +0200

    new content

--- JS Fundamentals/09.Using_Objects/05.Youngest-person.js
+++ JS Fundamentals/09.Using_Objects/05.Youngest-person.js
@@ -15,20 +15,43 @@
 // The full name of the youngest person
 
 function solve(args) {
-    console.log(args);
+    
+    var len = args.length,
+        array = [],
+        person = '',
+        people = getPeople(),
+        position = getYoungest(people);
+   
+    console.log(people[position].firstname + " " + people[position].lastname);
+
+    function getPeople() {
+        for (var i = 0; i < len; i++ ) {
+            if ( i % 3 === 0) {          
+                person = new Object();
+                person.firstname = args[i];
+                person.lastname = args[i+1];
+                person.age = +args[i+2];
+                array.push(person);
+            }
+        }
+        return array;
+    }
+
+    function getYoungest(object) {
+        var min = 999,
+            position;
+        for (var i = 0; i < object.length; i++) {
+            if (object[i].age < min) {
+                min = object[i].age;
+                position = i;
+            }
+        }
+        return position;
+    }
 }
 
 solve([ 'Gosho', 'Petrov', '32', 'Bay', 'Ivan', '81', 'John', 'Doe', '42' ]);
+// Gosho Petrov
 
-solve([ 'Penka',
-  'Hristova',
-  '61',
-  'System',
-  'Failiure',
-  '88',
-  'Bat',
-  'Man',
-  '16',
-  'Malko',
-  'Kote',
-  '72' ]);
+solve([ 'Penka', 'Hristova', '61', 'System', 'Failiure', '88', 'Bat', 'Man', '16', 'Malko', 'Kote', '72' ]);
+// Bat Man
